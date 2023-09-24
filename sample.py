8307fdb1f873d273344ea29749960a7a5096bcc3
gagan mylapilli <seryonton@gmail.com>
2023-09-24 10:39:05 +0530

    A sample file to be commited to loacl rep

---
+++ sample.py
@@ -0,0 +1,21 @@
+class node:
+    def __init__(self,data):
+        self.value=data
+        self.next=None
+class linkedlist:
+    def __init__(self):
+        self.headnode=None
+    def iterate(self):
+        pointer =self.headnode
+        while pointer:
+            print(str(pointer.value)+"->",end="")
+            pointer=pointer.next
+        print("None")
+        return 
+demo_lnkd_list=linkedlist()
+demo_lnkd_list.headnode=node(1)
+node2=node(2)
+node3=node(3)
+demo_lnkd_list.headnode.next=node2
+node2.next=node3
+demo_lnkd_list.iterate()
